4ac79b29568cecfc9080ca3f1a28cad7f446cb6d
CharKourkoulis <cult_bob@hotmail.com>
2020-04-14 21:04:10 +0300

    final commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { AuthModule } from './auth/auth.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthHttpInterceptor } from './auth/auth-http-interceptor';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AuthModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/inbox/email-index/email-index.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { EmailService } from '../email.service';
+import { Email } from '../email';
+
+@Component({
+  selector: 'app-email-index',
+  templateUrl: './email-index.component.html',
+  styleUrls: ['./email-index.component.css']
+})
+
+
+export class EmailIndexComponent implements OnInit {
+
+  emails = [];
+
+  constructor(private emailService: EmailService) { }
+
+  ngOnInit(): void {
+    this.emailService.getEmails()
+        .subscribe(emails => {
+          this.emails = emails;
+        });
+  }
+
+}
